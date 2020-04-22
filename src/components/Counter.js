c2806ec2dd0008761cf65ff991a4b5d0f508680c
Matt Cheung <matt.cheung@oocl.com>
2020-04-22 09:57:43 +0800

    code tab spacing formatting

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,41 +2,41 @@ import React, { Component } from 'react'
 import { INIT_COUNTER_VALUE, COUNTER_INCREMENT, COUNTER_DECREMENT } from '../constants/constants'
 
 export default class Counter extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.onIncrease = this.onIncrease.bind(this);
-        this.onDecrease = this.onDecrease.bind(this);
+    this.onIncrease = this.onIncrease.bind(this);
+    this.onDecrease = this.onDecrease.bind(this);
 
-        this.state = {
-            value: INIT_COUNTER_VALUE,
-        };
+    this.state = {
+      value: INIT_COUNTER_VALUE,
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.number !== prevProps.number) {
+      this.setState({ value: INIT_COUNTER_VALUE });
     }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (this.props.number !== prevProps.number) {
-            this.setState({ value: INIT_COUNTER_VALUE });
-        }
-        if (this.state.value != prevState.value) {
-            this.props.onCalculate(this.state.value - prevState.value);
-        }
+    if (this.state.value != prevState.value) {
+      this.props.onCalculate(this.state.value - prevState.value);
     }
+  }
 
-    onIncrease() {
-        this.setState((prevState) => ({ value: prevState.value + COUNTER_INCREMENT }));
-    }
+  onIncrease() {
+    this.setState((prevState) => ({ value: prevState.value + COUNTER_INCREMENT }));
+  }
 
-    onDecrease() {
-        this.setState((prevState) => ({ value: prevState.value + COUNTER_DECREMENT }));
-    }
+  onDecrease() {
+    this.setState((prevState) => ({ value: prevState.value + COUNTER_DECREMENT }));
+  }
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.onDecrease}>-</button>
-                <span>{this.state.value}</span>
-                <button onClick={this.onIncrease}>+</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <button onClick={this.onDecrease}>-</button>
+        <span>{this.state.value}</span>
+        <button onClick={this.onIncrease}>+</button>
+      </div>
+    )
+  }
 }
--- src/components/CounterGroup.js
+++ src/components/CounterGroup.js
@@ -3,55 +3,55 @@ import Counter from './Counter'
 import { INIT_COUNTER_SIZE, INIT_COUNTER_GROUP_SUM } from '../constants/constants'
 
 export default class CounterGroup extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.onChange = this.onChange.bind(this);
-        this.onCalculate = this.onCalculate.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onCalculate = this.onCalculate.bind(this);
 
-        this.state = {
-            size: INIT_COUNTER_SIZE,
-            sum: INIT_COUNTER_GROUP_SUM,
-        };
-    }
+    this.state = {
+      size: INIT_COUNTER_SIZE,
+      sum: INIT_COUNTER_GROUP_SUM,
+    };
+  }
 
-    initArray(size) {
-        return Array.from(Array(size).keys());
-    }
+  initArray(size) {
+    return Array.from(Array(size).keys());
+  }
 
-    onCalculate(delta) {
-        this.setState(prevState => ({ sum: prevState.sum + delta }));
-    }
+  onCalculate(delta) {
+    this.setState(prevState => ({ sum: prevState.sum + delta }));
+  }
 
-    onChange(event) {
-        const value = event.target.value;
-        this.setState({
-            size: value.length > 0 ? parseInt(value) : INIT_COUNTER_SIZE,
-            sum: INIT_COUNTER_GROUP_SUM
-        });
-    }
+  onChange(event) {
+    const value = event.target.value;
+    this.setState({
+      size: value.length > 0 ? parseInt(value) : INIT_COUNTER_SIZE,
+      sum: INIT_COUNTER_GROUP_SUM
+    });
+  }
 
-    render() {
-        let counters = this.initArray(this.state.size);
-        return (
-            <div>
-                <form>
-                    <fieldset>
-                        Generate
+  render() {
+    let counters = this.initArray(this.state.size);
+    return (
+      <div>
+        <form>
+          <fieldset>
+            Generate
                         <input onChange={this.onChange} type="text" value={this.state.size} />
-                        Counters
+            Counters
                         <p>
-                            Sum of all counters: {this.state.sum}
-                        </p>
-                    </fieldset>
-                </form>
-                {
-                    counters.map((value) => (<Counter
-                        key={value}
-                        number={this.state.number}
-                        onCalculate={this.onCalculate} />))
-                }
-            </div>
-        )
-    }
+              Sum of all counters: {this.state.sum}
+            </p>
+          </fieldset>
+        </form>
+        {
+          counters.map((value) => (<Counter
+            key={value}
+            number={this.state.number}
+            onCalculate={this.onCalculate} />))
+        }
+      </div>
+    )
+  }
 }
